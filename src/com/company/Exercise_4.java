186e43de7703165268f8a7b83e767bc739bf116b
Arman <avalyanarman@gmail.com>
2021-04-21 21:06:19 +0400

    Exercise_4

---
+++ src/com/company/Exercise_4.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Exercise_4 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number of the array: ");
+        int firstNumber = scanner.nextInt();
+        System.out.print("Enter the length of the array: ");
+        int lengthArray = scanner.nextInt();
+        int[] arrayOfMultiples = new int[lengthArray];
+
+        for (int i = 0; i < arrayOfMultiples.length; i++) {
+            arrayOfMultiples[i] = firstNumber + i * firstNumber;
+        }
+        System.out.print("(" + firstNumber + ", " + lengthArray + ") → " + Arrays.toString(arrayOfMultiples));
+    }
+}
+
